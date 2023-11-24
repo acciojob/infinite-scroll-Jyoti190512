0afa5dc6fe91be1b9b586110197dab85ddfc22c0
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-11-24 10:37:57 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function () {
+    const infiList = document.getElementById("infi-list");
+
+    // Function to add new list items
+    function addListItem() {
+        const newItem = document.createElement("li");
+        newItem.textContent = "List item " + (infiList.childElementCount + 1);
+        infiList.appendChild(newItem);
+    }
+
+    // Function to check if the user has reached the bottom of the list
+    function isScrolledToBottom() {
+        return infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight;
+    }
+
+    // Function to handle scroll events
+    function handleScroll() {
+        if (isScrolledToBottom()) {
+            // When user reaches the bottom of the list, add new list items
+            addListItem();
+            addListItem();
+        }
+    }
+
+    // Add initial list items
+    for (let i = 1; i <= 10; i++) {
+        addListItem();
+    }
+
+    // Add event listener for scrolling
+    infiList.addEventListener("scroll", handleScroll);
+});
+
 
